7693ce8398cb03cd1f1871c241559e707b8fab6c
taka9999 <to0t0seki@gmail.com>
2025-01-02 15:43:04 +0900

    2commit

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-from threading import Thread
+print("Hello World")
+print("Hello World")
 
-print("Hello World")
-print("Hello World")
-t = Thread(target=print, args=("Hello World",))
-t.start()
-t.join()
